8814124aaf7f38d9776cd38177f763e9472847e7
Jenkins Shubham <jenkinsshubham@gmail.com>
2016-05-14 22:48:58 +0530

    feeds display problems fixed

--- Body/include/feeds.php
+++ Body/include/feeds.php
@@ -7,22 +7,25 @@
                         <a class="btn" data-popup-open="popup-<?php echo $i ?>" href="#"><b><?php echo $notice['title'] ?></b></a>
                     </div>
                     <div class="feed_item_description"><?php echo $notice['content'] ?></div>
-                    <div class="feed_item_date">Publisher: <<?php echo $notice['publisher'] ?></div>
                     <div class="feed_item_date"><?php echo $notice['time'] ?></div>
                 </div>
-                </div><!--#itemlist-->
+                
                        <!-- POPUP NOTICE -->
                 <div class="popup" data-popup="popup-<?php echo $i ?>">
                     <div class="popup-inner">
                         <div style="background: #fff;height: 100%;width: 100%;border-radius:8px;padding: 10px;">
-                            <h1><b><?php echo $notice['title'] ?></b></h1>
-                            <div style="width: 80%"><?php echo $notice['content'] ?></div>
-                            <p><i><?php echo $notice['time'] ?></i></p>
+                        	<div class="panel panel-warning">
+	                            <div class="panel-heading"><big class="panel-title"><b><?php echo $notice['title'] ?></b></big></div>
+	                            <div class="panel-body"><h4><?php echo $notice['content'] ?></h4></div>
+	                            <hr/>
+	                            <p><i> Published by: <?php echo $notice['publisher'] ?></i></p>
+	                            <p><i> Time: <?php echo $notice['time'] ?></i></p>
+	                        </div>
                         </div>
                         <a class="popup-close" data-popup-close="popup-<?php echo $i ?>" href="#">x</a>
                     </div>
                 </div>
             <?php ; $i++; } ?>
+        </div><!--#itemlist-->
     </div><!--#content and #flexcroll-->
-    <div id="footer"></div>
 </div><!--#container-->
