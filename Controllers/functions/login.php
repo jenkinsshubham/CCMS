2dd1e02f578e47e8901b78a16996e98be52ebbc9
Jenkins Shubham <jenkinsshubham@gmail.com>
2016-05-21 00:47:37 +0530

    login only available for approved users

--- Controllers/functions/login.php
+++ Controllers/functions/login.php
@@ -4,36 +4,39 @@ if (isset($_POST['submit'])) {
 	$password = $db->real_escape_string($_POST['password']);
 	$frm =$db->real_escape_string($_POST['frm']);
 	// QUERYING
-	$sql="SELECT * ";
-	if ($frm=='s') 
-		$sql.="FROM log_s";
-	else 
-		$sql.=" FROM log_f";
-		$sql.=" WHERE username='$id' OR";
-	if ($frm=='s') 
-		$sql.=" usn='$id'";
-	else 
-		$sql.=" fid='$id'";
+	$sql="SELECT * FROM log_";
+	$sql.= ($frm=='s')?"s":"f";
+	$sql.=" WHERE username='$id' OR ";
+	$sql.=($frm=='s') ?"usn='$id'":"fid='$id'";
 
 	$result=$db->query($sql);
 
 	if (!$result) {
 		die('There was an error running the query ['.$db->error.']');
 	}
-
-	// FETCHING
-	$row=$result->fetch_array();	
-	$logid=$row['username'];
-
-	if($row['password']==$password){
-		$_SESSION['id'] = $logid;
-		$_SESSION['frm'] = $frm;
-		header("Location: ".BASEPATH);
+	if($result->num_rows==0) {
+		echo "<script>alert('User not registered!')</script>";
 	}
-	else{
-		?>
-		<script>alert('wrong details');</script>
-		<?php
+	else {
+
+		// FETCHING
+		$row=$result->fetch_array();	
+		$logid=$row['username'];
+		$flag=$row['flag'];
+
+		if($row['password']==$password){
+			if ($flag) {
+				$_SESSION['id'] = $logid;
+				$_SESSION['frm'] = $frm;
+				header("Location: ".BASEPATH);
+			}
+			else echo "<script>alert('Account not approved!')</script>";
+		}
+		else{
+			?>
+			<script>alert('Invalid Password!');</script>
+			<?php
+		}
 	}
 
 }
