ead3a6934ca8c56fbec045c09b41db7cfddef924
Jenkins Shubham <jenkinsshubham@gmail.com>
2016-05-28 10:28:35 +0530

    clean url in user end

--- Body/include/user_bar.php
+++ Body/include/user_bar.php
@@ -2,10 +2,13 @@
   <div class='panel-heading'> <h3 class='panel-title'>My Account</h3> </div>
   <div class='panel-body' style='padding:12px;'>
     <ul class='nav navbar-nav'>
-      <li><a href='?profile'><span class='glyphicon glyphicon-user' aria-hidden='true'></span>My Profile</a></li>
+      <li>
+        <a href='<?php echo BASEPATH ?>page/profile'>
+          <span class='glyphicon glyphicon-user' aria-hidden='true'></span>My Profile</a>
+      </li>
       <li><a href='#'><span class='glyphicon glyphicon-envelope' aria-hidden='true'></span> Messages</a></li>
-      <li><a href='?assignments#assignments'>Assignments</a></li>
-      <li><a href='?attendence'>Attendence</a></li>
+      <li><a href='#'>Assignments</a></li>
+      <li><a href='#'>Attendence</a></li>
       <li><a href='logout'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span> Logout</span></a></li>
       </ul>
   </div>
