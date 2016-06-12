1e47a34512e0bf21d7528bac84c387144f351c05
Jenkins Shubham <jenkinsshubham@gmail.com>
2016-06-13 00:45:54 +0530

    mailer

--- Controllers/functions/mailer.php
+++ Controllers/functions/mailer.php
@@ -2,6 +2,22 @@
 
 require_once(CONTROLLERS.'lib/swift/swift_required.php');
 
+// Creating Transport
+$transport=Swift_SmtpTransport::newInstance('smtp.gmail.com')
+	->setUsername('imessit@gmail.com')
+	->setPassword('sasy@123');
+
+//  Creating Mailer
+$mailer=Swift_Mailer::newInstance($transport);
+// Creating Message
+$message=Swift_Message::newInstance('$Subject')
+	->setFrom(array('imessit@gmail.com'=>'SIT'))
+	->setSender('imessit@gmail.com')
+	->setTo($To)
+	->setBody($HtmlMessage,'text/html');
+	->addPart($PlainMessage.'text/plain')
+;
+
 
 
 ?>
