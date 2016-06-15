167f02d97ed66b4f0ef691db18a8556ea00eeb26
Jenkins Shubham <jenkinsshubham@gmail.com>
2016-06-15 18:06:05 +0530

    error handler used in addnotice by admin

--- Controllers/functions/add_notice.php
+++ Controllers/functions/add_notice.php
@@ -12,13 +12,19 @@ if(isset($_POST['submit'])){
 	$result=$db->query($sql);
 
 	if (!$result) {
-		echo('<big/>Publishing ERROR!!</big> <br/> ['.$db->error.']');
+		$_SESSION['_m']="Error while publishing Notice..!";
+		$_SESSION['_mb']="Check and try Again.";
+		$_SESSION['_t']='d';
+		echo "<script>window.location.assign(window.location.href);</script>";
 	}
 
 
 	if($result){
-		echo "<script>alert('Notice published successfully!');</script>";
-		echo "Notice published successfully!";
+		$_SESSION['_m']="Notice Published successfully.";
+		$_SESSION['_mb']="Title: $title<br/>";
+		$_SESSION['_mb'].="$content<br/>";
+		$_SESSION['_t']='s';
+		echo "<script>window.location.assign('".BASEPATH."');</script>";
 	}
 }
 ?>
