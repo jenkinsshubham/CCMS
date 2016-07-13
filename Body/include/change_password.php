c16ecd1d0a0204943ccfabba9ca36fac73d9734e
Jenkins Shubham <jenkinsshubham@gmail.com>
2016-07-13 11:13:18 +0530

    change password working with validation

--- Body/include/change_password.php
+++ Body/include/change_password.php
@@ -1,16 +1,18 @@
 <?php require CONTROLLERS.'functions/change_password.php';?>
+<script type="text/javascript" src="<?php echo STYLERS ?>js/validation.js"></script>
 <div class="panel panel-default">
 	<div class="panel-heading"><h1>Change Password</h1></div>
 	<div class="panel-body">
 		<form method="POST">
-			<div class="input-group">
+			<!-- <div class="input-group">
 				<span class="input-group-addon"> Current Password:</span>
 				<input class="form-control" type="password" placeholder="Enter your current password..." name="pass">
-			</div><br/>
+			</div> --><br/>
 			<div class="input-group">
 				<span class="input-group-addon"> New Password:</span>
-				<input class="form-control" type="password" placeholder="Enter a strong new password.." name="newpass">
-				<input class="form-control" type="password" placeholder="Re-enter new password..." name="">
+				<input class="form-control" type="password" 
+					placeholder="Enter a strong new password.." id="password" name="newpass">
+				<input class="form-control" type="password" placeholder="Re-enter new password..." id="password_confirm">
 			</div><br/>
 			<input class="btn btn-primary pull-right" type="submit" value="Change password" name="submit">
 		</form>
