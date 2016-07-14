88bc747e3ca5e25e51e88098ebe3a358d758373c
Jenkins Shubham <jenkinsshubham@gmail.com>
2016-07-14 21:20:16 +0530

    search no result fixed; extra bugs fixed

--- Controllers/functions/search.php
+++ Controllers/functions/search.php
@@ -10,27 +10,32 @@ if(isset($_GET['key'])||isset($_POST['key'])){
 	    $array = array();
 
 		$result=$db->query("select * from $tbl where name LIKE '%{$key}%'");
-	    while($row=$result->fetch_array()){
-	    	$results.="<a href=\"".BASEPATH."profile/".$row['username']."\">";
+		if($result->num_rows>0){
+		    while($row=$result->fetch_array()){
+		    	$results.="<a href=\"".BASEPATH."profile/".$row['username']."\">";
 
-	    	// AVATAR
-			$_img=$row['img'];
-			$__sex=$row['sex'];
-				if(empty($_img)){
-					$_avatar = STYLERS.'images/avatars/';
-						if ($_frm=='s') {
-							$_avatar .= ($__sex=='M') ? 'sm' : 'sf' ;
+		    	// AVATAR
+				$_img=$row['img'];
+				$__sex=$row['sex'];
+					if(empty($_img)){
+						$_avatar = STYLERS.'images/avatars/';
+							if ($_frm=='s') {
+								$_avatar .= ($__sex=='M') ? 'sm' : 'sf' ;
+							}
+							else $_avatar .= ($__sex=='M') ? 'fm' : 'ff' ;
+							$_avatar.='.png';
+						
 						}
-						else $_avatar .= ($__sex=='M') ? 'fm' : 'ff' ;
-						$_avatar.='.png';
-					
-					}
-				else {if(!empty($_img)) $_avatar="../User_Uploads/profile/".$_img;}
-				$s_e_m=($_frm=='s')?("sem:".$row['sem']." branch:"):"";
-			$results.="<div><img alt=\"User Pic\" src=\"".$_avatar."\" style=\"width:50px;height:50px;\" class=\"img-circle\"> ";
-			$results.=$row['name']." <small>(".$s_e_m.$row['br'].")</small> </div></a><hr/>";
+					else {if(!empty($_img)) $_avatar="../User_Uploads/profile/".$_img;}
+					$s_e_m=($_frm=='s')?("sem:".$row['sem']." branch:"):"";
+				$results.="<div><img alt=\"User Pic\" src=\"".$_avatar."\" style=\"width:50px;height:50px;\" class=\"img-circle\"> ";
+				$results.=$row['name']." <small>(".$s_e_m.$row['br'].")</small> </div></a><hr/>";
+			}
+		}
+		else {
+			$results.="No results found!";
+		}
 
 
-}
 }
 ?>
